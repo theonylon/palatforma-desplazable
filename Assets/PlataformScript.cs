7b7c9b8ecb88cb3411a3cdc8a819b4c665d33e23
theonlyone <102159023+theonylon@users.noreply.github.com>
2022-05-23 10:32:21 -0300

    job terminated

---
+++ Assets/PlataformScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformScript : MonoBehaviour
+{
+    public float xMovementSpeed;
+    public float posLim;
+    public float leftLim;
+    public bool toRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (toRight == true)
+        {
+            transform.position += new Vector3(xMovementSpeed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(xMovementSpeed, 0, 0);
+        }
+
+        if (transform.position.x > posLim)
+
+        {
+            toRight = false;
+        }
+       else if (transform.position.x < leftLim)
+        {
+            toRight = true;
+        }
+
+    }
+}
